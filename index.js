55b2fb8187a30694fb5b519eee9f8fe531455e78
Gianni Baccino <gianni.baccino@streaver.com>
2026-02-18 12:45:47 -0300

    test file modified

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-function add(a, b) {
-  return a + b;
+function add(a, b, c) {
+  console.log("Adding 3 numbers");
+  return a + b + c;
 }
 
 function subtract(a, b) {
+  console.log("Subtracting two numbers");
   return a - b;
 }
 
 function multiply(a, b) {
-  return a * b;
+  console.log("Multiplying two numbers");
+  let result = 0;
+  for (let i = 0; i < b; i++) {
+    result += a;
+  }
+  return result;
 }
