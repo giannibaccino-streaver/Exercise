861d6f155331183fb4b109248b4f5859fe06293d
Gianni Baccino <gianni.baccino@streaver.com>
2026-02-18 12:52:22 -0300

    test file modified for conflict

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 function add(a, b) {
+  console.log("Adding numbers");
   return a + b;
 }
 
 function subtract(a, b) {
+  console.log("Subtracting numbers");
   return a - b;
 }
 
 function multiply(a, b) {
+  console.log("Multiplying numbers");
   return a * b;
 }
 
