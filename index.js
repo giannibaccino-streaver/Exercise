316828a30dfaf6488a9578f3f29dc5d10feb668e
Gianni Baccino <gianni.baccino@streaver.com>
2026-02-18 12:49:38 -0300

    test file created

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+// Conflicting version on branch c2
 function add(a, b) {
-  return a + b;
+  console.log("c2: adding", a, "and", b);
+  return a + b + 1;
 }
 
 function subtract(a, b) {
-  return a - b;
+  console.log("c2: subtracting", a, "and", b);
+  return a - b - 1;
 }
 
 function multiply(a, b) {
-  return a * b;
+  console.log("c2: multiplying", a, "and", b);
+  let result = 0;
+  for (let i = 0; i < b; i += 1) {
+    result += a;
+  }
+  return result;
 }
-
-module.exports = {
-  add,
-  subtract,
-  multiply,
-};
